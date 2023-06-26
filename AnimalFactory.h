d21eacb8de2662e1d374cd57eb6a4b0d061ff687
Moshe Berman <moshe@foreflight.com>
2023-06-26 10:38:18 -0400

    Add platform name to user header search paths.

--- AnimalFactory.h
+++ AnimalFactory.h
@@ -13,9 +13,8 @@
 //#import <TestPackage/Sources/TestPackage/>
 //#import "/Users/bill/Library/Developer/Xcode/DerivedData/SimpleApp-axfzvryccojhvubnatlnnuopoyyf/Build/Intermediates.noindex/TestPackage.build/Debug/TestPackage.build/Objects-normal/arm64/TestPackage-Swift.h"
 
-//This works after adding the following to user search paths: ${OBJROOT}/GeneratedModuleMaps
-//#import "TestPackage-Swift.h"
-#import "/Users/bill/Library/Developer/Xcode/DerivedData/SimpleApp-axfzvryccojhvubnatlnnuopoyyf/Build/Intermediates.noindex/GeneratedModuleMaps-iphonesimulator/TestPackage-Swift.h"
+//This works after adding the following to user search paths: ${OBJROOT}/GeneratedModuleMaps$(EFFECTIVE_PLATFORM_NAME)
+#import "TestPackage-Swift.h"
 
 #import <Foundation/Foundation.h>
 
